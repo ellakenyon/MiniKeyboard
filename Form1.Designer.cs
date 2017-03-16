34e0b97bc2ce05ecbf69038f37d8074277c2c157
ellakenyon <b6039218@my.shu.ac.uk>
2017-03-16 15:26:07 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NotePadTextBox = new System.Windows.Forms.RichTextBox();
+            this.ModeBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.PreviousBtn = new System.Windows.Forms.Button();
+            this.PredictBtn = new System.Windows.Forms.Button();
+            this.BlankBtn1 = new System.Windows.Forms.Button();
+            this.No7Btn = new System.Windows.Forms.Button();
+            this.No8Btn = new System.Windows.Forms.Button();
+            this.No9Btn = new System.Windows.Forms.Button();
+            this.No4Btn = new System.Windows.Forms.Button();
+            this.No5Btn = new System.Windows.Forms.Button();
+            this.No6Btn = new System.Windows.Forms.Button();
+            this.BlankBtn2 = new System.Windows.Forms.Button();
+            this.No1Btn = new System.Windows.Forms.Button();
+            this.No2Btn = new System.Windows.Forms.Button();
+            this.No3Btn = new System.Windows.Forms.Button();
+            this.EnterBtn = new System.Windows.Forms.Button();
+            this.AsteriskBtn = new System.Windows.Forms.Button();
+            this.ZeroBtn = new System.Windows.Forms.Button();
+            this.HashtagBtn = new System.Windows.Forms.Button();
+            this.Builder = new System.Windows.Forms.TextBox();
+            this.ModeStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NotePadTextBox
+            // 
+            this.NotePadTextBox.Location = new System.Drawing.Point(12, 12);
+            this.NotePadTextBox.Name = "NotePadTextBox";
+            this.NotePadTextBox.Size = new System.Drawing.Size(565, 177);
+            this.NotePadTextBox.TabIndex = 0;
+            this.NotePadTextBox.Text = "";
+            // 
+            // ModeBtn
+            // 
+            this.ModeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ModeBtn.Location = new System.Drawing.Point(183, 255);
+            this.ModeBtn.Name = "ModeBtn";
+            this.ModeBtn.Size = new System.Drawing.Size(53, 53);
+            this.ModeBtn.TabIndex = 1;
+            this.ModeBtn.Text = "Mode";
+            this.ModeBtn.UseVisualStyleBackColor = false;
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.NextBtn.Location = new System.Drawing.Point(242, 255);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(53, 53);
+            this.NextBtn.TabIndex = 2;
+            this.NextBtn.Text = "Next";
+            this.NextBtn.UseVisualStyleBackColor = false;
+            // 
+            // PreviousBtn
+            // 
+            this.PreviousBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.PreviousBtn.Location = new System.Drawing.Point(301, 255);
+            this.PreviousBtn.Name = "PreviousBtn";
+            this.PreviousBtn.Size = new System.Drawing.Size(53, 53);
+            this.PreviousBtn.TabIndex = 3;
+            this.PreviousBtn.Text = "Previous";
+            this.PreviousBtn.UseVisualStyleBackColor = false;
+            // 
+            // PredictBtn
+            // 
+            this.PredictBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.PredictBtn.Location = new System.Drawing.Point(360, 255);
+            this.PredictBtn.Name = "PredictBtn";
+            this.PredictBtn.Size = new System.Drawing.Size(53, 53);
+            this.PredictBtn.TabIndex = 4;
+            this.PredictBtn.Text = "Show Prediction";
+            this.PredictBtn.UseVisualStyleBackColor = false;
+            // 
+            // BlankBtn1
+            // 
+            this.BlankBtn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BlankBtn1.Location = new System.Drawing.Point(360, 314);
+            this.BlankBtn1.Name = "BlankBtn1";
+            this.BlankBtn1.Size = new System.Drawing.Size(53, 53);
+            this.BlankBtn1.TabIndex = 5;
+            this.BlankBtn1.UseVisualStyleBackColor = false;
+            // 
+            // No7Btn
+            // 
+            this.No7Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No7Btn.Location = new System.Drawing.Point(183, 314);
+            this.No7Btn.Name = "No7Btn";
+            this.No7Btn.Size = new System.Drawing.Size(53, 53);
+            this.No7Btn.TabIndex = 6;
+            this.No7Btn.Text = "7";
+            this.No7Btn.UseVisualStyleBackColor = false;
+            // 
+            // No8Btn
+            // 
+            this.No8Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No8Btn.Location = new System.Drawing.Point(242, 314);
+            this.No8Btn.Name = "No8Btn";
+            this.No8Btn.Size = new System.Drawing.Size(53, 53);
+            this.No8Btn.TabIndex = 7;
+            this.No8Btn.Text = "8";
+            this.No8Btn.UseVisualStyleBackColor = false;
+            // 
+            // No9Btn
+            // 
+            this.No9Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No9Btn.Location = new System.Drawing.Point(301, 314);
+            this.No9Btn.Name = "No9Btn";
+            this.No9Btn.Size = new System.Drawing.Size(53, 53);
+            this.No9Btn.TabIndex = 8;
+            this.No9Btn.Text = "9";
+            this.No9Btn.UseVisualStyleBackColor = false;
+            // 
+            // No4Btn
+            // 
+            this.No4Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No4Btn.Location = new System.Drawing.Point(183, 373);
+            this.No4Btn.Name = "No4Btn";
+            this.No4Btn.Size = new System.Drawing.Size(53, 53);
+            this.No4Btn.TabIndex = 9;
+            this.No4Btn.Text = "4";
+            this.No4Btn.UseVisualStyleBackColor = false;
+            // 
+            // No5Btn
+            // 
+            this.No5Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No5Btn.Location = new System.Drawing.Point(242, 373);
+            this.No5Btn.Name = "No5Btn";
+            this.No5Btn.Size = new System.Drawing.Size(53, 53);
+            this.No5Btn.TabIndex = 10;
+            this.No5Btn.Text = "5";
+            this.No5Btn.UseVisualStyleBackColor = false;
+            // 
+            // No6Btn
+            // 
+            this.No6Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No6Btn.Location = new System.Drawing.Point(301, 373);
+            this.No6Btn.Name = "No6Btn";
+            this.No6Btn.Size = new System.Drawing.Size(53, 53);
+            this.No6Btn.TabIndex = 11;
+            this.No6Btn.Text = "6";
+            this.No6Btn.UseVisualStyleBackColor = false;
+            // 
+            // BlankBtn2
+            // 
+            this.BlankBtn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BlankBtn2.Location = new System.Drawing.Point(360, 373);
+            this.BlankBtn2.Name = "BlankBtn2";
+            this.BlankBtn2.Size = new System.Drawing.Size(53, 53);
+            this.BlankBtn2.TabIndex = 12;
+            this.BlankBtn2.UseVisualStyleBackColor = false;
+            // 
+            // No1Btn
+            // 
+            this.No1Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No1Btn.Location = new System.Drawing.Point(183, 432);
+            this.No1Btn.Name = "No1Btn";
+            this.No1Btn.Size = new System.Drawing.Size(53, 53);
+            this.No1Btn.TabIndex = 13;
+            this.No1Btn.Text = "1";
+            this.No1Btn.UseVisualStyleBackColor = false;
+            // 
+            // No2Btn
+            // 
+            this.No2Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No2Btn.Location = new System.Drawing.Point(242, 432);
+            this.No2Btn.Name = "No2Btn";
+            this.No2Btn.Size = new System.Drawing.Size(53, 53);
+            this.No2Btn.TabIndex = 14;
+            this.No2Btn.Text = "2";
+            this.No2Btn.UseVisualStyleBackColor = false;
+            // 
+            // No3Btn
+            // 
+            this.No3Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.No3Btn.Location = new System.Drawing.Point(301, 432);
+            this.No3Btn.Name = "No3Btn";
+            this.No3Btn.Size = new System.Drawing.Size(53, 53);
+            this.No3Btn.TabIndex = 15;
+            this.No3Btn.Text = "3";
+            this.No3Btn.UseVisualStyleBackColor = false;
+            // 
+            // EnterBtn
+            // 
+            this.EnterBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.EnterBtn.Location = new System.Drawing.Point(360, 432);
+            this.EnterBtn.Name = "EnterBtn";
+            this.EnterBtn.Size = new System.Drawing.Size(53, 112);
+            this.EnterBtn.TabIndex = 16;
+            this.EnterBtn.Text = "Enter";
+            this.EnterBtn.UseVisualStyleBackColor = false;
+            // 
+            // AsteriskBtn
+            // 
+            this.AsteriskBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.AsteriskBtn.Location = new System.Drawing.Point(183, 491);
+            this.AsteriskBtn.Name = "AsteriskBtn";
+            this.AsteriskBtn.Size = new System.Drawing.Size(53, 53);
+            this.AsteriskBtn.TabIndex = 17;
+            this.AsteriskBtn.Text = "* - _";
+            this.AsteriskBtn.UseVisualStyleBackColor = false;
+            // 
+            // ZeroBtn
+            // 
+            this.ZeroBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ZeroBtn.Location = new System.Drawing.Point(242, 491);
+            this.ZeroBtn.Name = "ZeroBtn";
+            this.ZeroBtn.Size = new System.Drawing.Size(53, 53);
+            this.ZeroBtn.TabIndex = 18;
+            this.ZeroBtn.Text = "000";
+            this.ZeroBtn.UseVisualStyleBackColor = false;
+            // 
+            // HashtagBtn
+            // 
+            this.HashtagBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.HashtagBtn.Location = new System.Drawing.Point(301, 491);
+            this.HashtagBtn.Name = "HashtagBtn";
+            this.HashtagBtn.Size = new System.Drawing.Size(53, 53);
+            this.HashtagBtn.TabIndex = 19;
+            this.HashtagBtn.Text = "#";
+            this.HashtagBtn.UseVisualStyleBackColor = false;
+            // 
+            // Builder
+            // 
+            this.Builder.Location = new System.Drawing.Point(183, 229);
+            this.Builder.Name = "Builder";
+            this.Builder.Size = new System.Drawing.Size(147, 20);
+            this.Builder.TabIndex = 20;
+            // 
+            // ModeStatus
+            // 
+            this.ModeStatus.Location = new System.Drawing.Point(336, 229);
+            this.ModeStatus.Name = "ModeStatus";
+            this.ModeStatus.ReadOnly = true;
+            this.ModeStatus.Size = new System.Drawing.Size(77, 20);
+            this.ModeStatus.TabIndex = 21;
+            this.ModeStatus.Text = "Multi-Press";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 597);
+            this.Controls.Add(this.ModeStatus);
+            this.Controls.Add(this.Builder);
+            this.Controls.Add(this.HashtagBtn);
+            this.Controls.Add(this.ZeroBtn);
+            this.Controls.Add(this.AsteriskBtn);
+            this.Controls.Add(this.EnterBtn);
+            this.Controls.Add(this.No3Btn);
+            this.Controls.Add(this.No2Btn);
+            this.Controls.Add(this.No1Btn);
+            this.Controls.Add(this.BlankBtn2);
+            this.Controls.Add(this.No6Btn);
+            this.Controls.Add(this.No5Btn);
+            this.Controls.Add(this.No4Btn);
+            this.Controls.Add(this.No9Btn);
+            this.Controls.Add(this.No8Btn);
+            this.Controls.Add(this.No7Btn);
+            this.Controls.Add(this.BlankBtn1);
+            this.Controls.Add(this.PredictBtn);
+            this.Controls.Add(this.PreviousBtn);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.ModeBtn);
+            this.Controls.Add(this.NotePadTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox NotePadTextBox;
+        private System.Windows.Forms.Button ModeBtn;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Button PreviousBtn;
+        private System.Windows.Forms.Button PredictBtn;
+        private System.Windows.Forms.Button BlankBtn1;
+        private System.Windows.Forms.Button No7Btn;
+        private System.Windows.Forms.Button No8Btn;
+        private System.Windows.Forms.Button No9Btn;
+        private System.Windows.Forms.Button No4Btn;
+        private System.Windows.Forms.Button No5Btn;
+        private System.Windows.Forms.Button No6Btn;
+        private System.Windows.Forms.Button BlankBtn2;
+        private System.Windows.Forms.Button No1Btn;
+        private System.Windows.Forms.Button No2Btn;
+        private System.Windows.Forms.Button No3Btn;
+        private System.Windows.Forms.Button EnterBtn;
+        private System.Windows.Forms.Button AsteriskBtn;
+        private System.Windows.Forms.Button ZeroBtn;
+        private System.Windows.Forms.Button HashtagBtn;
+        private System.Windows.Forms.TextBox Builder;
+        private System.Windows.Forms.TextBox ModeStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form {
+
+        bool mode = true; //if true then in multi press mode else false in prediction mode.
+         
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
